3483b9cbbae393fae49259e4e2fa49a95ba93808
dangerous3 <dangerous3@yandex.ru>
2020-02-26 16:40:08 +0300

    Итоговое решения задачи 9-го степа  из модуля 1.5 (реализация буфера)

--- m1.5_classes-intro/1.5-buffer.py
+++ m1.5_classes-intro/1.5-buffer.py
@@ -1,38 +1,40 @@
+'''Вам дается последовательность целых чисел и вам нужно ее обработать и вывести на экран сумму первой пятерки чисел
+из этой последовательности, затем сумму второй пятерки, и т. д.
+
+Но последовательность не дается вам сразу целиком. С течением времени к вам поступают её последовательные части.
+Например, сначала первые три элемента, потом следующие шесть, потом следующие два и т. д.
+
+Реализуйте класс Buffer, который будет накапливать в себе элементы последовательности и выводить сумму пятерок
+последовательных элементов по мере их накопления.
+
+Одним из требований к классу является то, что он не должен хранить в себе больше элементов, чем ему действительно
+необходимо, т. е. он не должен хранить элементы, которые уже вошли в пятерку, для которой была выведена сумма.'''
+
 class Buffer():
     def __init__(self):
         # конструктор без аргументов
         self.buffer = []
-        self.temp = []
 
     def add(self, *a):
-        if len(self.buffer) <= 5:
-            for i in range(len(a)):
-                self.temp.append(a[i])
-            temp_range = range(len(self.temp))
-            for j in temp_range:
-                if len(self.buffer) < 5:
-                    self.buffer.append(self.temp[0])
-                    if len(self.temp) > 1:
-                        self.temp = self.temp[1:]
-            print(self.temp)
-            print(sum(self.buffer))
-
-
-        else:
+        for i in range(len(a)):
+            self.buffer.append(a[i])
+            if len(self.buffer) == 5:
                 print(sum(self.buffer))
                 self.buffer.clear()
-
 
     # добавить следующую часть последовательности
 
     def get_current_part(self):
         # вернуть сохраненные в текущий момент элементы последовательности в порядке, в котором они были
         # добавлены
-        print(self.buffer)
-        #print(self.temp)
+        return print(self.buffer)
 
 buf = Buffer()
 buf.add(1, 2, 3)
 buf.get_current_part()
 buf.add(4, 5, 6)
+buf.get_current_part()
+buf.add(7, 8, 9, 10)
+buf.get_current_part()
+buf.add(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
 buf.get_current_part()
