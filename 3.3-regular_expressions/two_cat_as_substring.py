2b743e5468e6ad1caa0366df950c4b8b1d5d5305
dangerous3 <dangerous3@yandex.ru>
2020-03-05 19:10:01 +0300

    Решение задачи 7-го степа  из модуля 3.3 (Регулярные выражения)

---
+++ 3.3-regular_expressions/two_cat_as_substring.py
@@ -0,0 +1,36 @@
+'''Вам дана последовательность строк.
+Выведите строки, содержащие "cat" в качестве подстроки хотя бы два раза.
+
+Примечание:
+Считать все строки по одной из стандартного потока ввода вы можете, например, так
+
+import sys
+
+for line in sys.stdin:
+    line = line.rstrip()
+    # process line
+
+
+Sample Input:
+
+catcat
+cat and cat
+catac
+cat
+ccaatt
+
+Sample Output:
+
+catcat
+cat and cat
+'''
+
+import sys
+import re
+
+for line in sys.stdin:
+    if line:
+        line = line.rstrip()
+        if re.search('.*cat.*cat.*', line, re.IGNORECASE):
+            print(line)
+
