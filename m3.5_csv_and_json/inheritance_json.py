501127bfceba17cdf9196a05237cf815ff3b478f
dangerous3 <dangerous3@yandex.ru>
2020-03-11 20:23:40 +0300

    Итоговое решение задачи 4-го степа  из модуля 3.5 (JSON и наследование)

--- m3.5_csv_and_json/inheritance_json.py
+++ m3.5_csv_and_json/inheritance_json.py
@@ -1,38 +1,46 @@
 import json
-from collections import defaultdict
 
 js = json.loads(input())
 
-print(js)
+#print(js)
 
-sibling = []
-inverted = {}
+res = {}
+graph= {}
 
-# Конвертирование в структуру данных:
-# inverted = {ancestor : [list of siblings]}
 for dct in js:
-    if len(dct['parents']) != 0:
-        for parent in dct['parents']:
-           if inverted.get(parent) == None:
-                inverted[parent] = [dct['name']]
-           else:
-               inverted[parent].append(dct['name'])
+    for parent in dct['parents']:
+        if parent not in graph:
+            graph[parent] = [dct['name']]
+        else:
+            graph[parent].append(dct['name'])
 
 
-print(inverted)
 
-# Функция нахождения всех путей между двумя нодами
+# Функция нахождения пути между двумя нодами
 # Используется техника backtracking (см. http://www.infocity.kiev.ua/prog/python/content/pytonesse_3.shtml)
-def find_all_paths(graph, start, end, path=[]):
+def find_path(graph, start, end, path=[]):
     path = path + [start]
-    if start == end:
-        return [path]
     if start not in graph:
-        return []
-    paths = []
+        return None
+    if start == end:
+        return path
     for node in graph[start]:
         if node not in path:
-            newpaths = find_all_paths(graph, node, end, path)
-            for newpath in newpaths:
-                paths.append(newpath)
-    return paths
+            new_path = find_path(graph, node, end, path)
+            if new_path:
+                return new_path
+    return None
+
+for dct in js:
+    if dct['name'] not in graph:
+        graph[dct['name']] = []
+
+for start in graph:
+    res[start] = 0
+    for end in graph:
+        if find_path(graph, start, end):
+            res[start] += 1
+
+for i in sorted(res):
+    print('{} : {}'.format(i, res[i]))
+
